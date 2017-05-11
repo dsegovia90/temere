ddfd636d524701aefe9142ed3fb9f986886dd537
Daniel Segovia <daniel@dabici.mx>
2017-05-10 21:31:36 -0500

    made a hello world for slack, missing permision to delete an app from rhinos to add ours

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,6 +1,11 @@
 
 module.exports = function(app){
 
-	
-	
+	app.get('/temere', function(req, res){
+		var slackResponse = {}
+		slackResponse.response_type = 'in_channel'
+		slackResponse.text = 'Hello rhinos!'
+		res.type('application/json').json(slackResponse).end()
+	})
+
 }
