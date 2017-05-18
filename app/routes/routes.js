1ce9513f38a36fedff06d416aeca0efc71f42b08
Daniel Segovia <daniel@dabici.mx>
2017-05-18 09:35:28 -0500

    changed response.text a bit

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -33,7 +33,7 @@ module.exports = function(app){
 
 		var slackResponse = {}
 		slackResponse.response_type = 'in_channel'
-		slackResponse.text = `Hello ${req.body.user_name}! This is your random name: *${randomAdj} ${randomNoun}*`
+		slackResponse.text = `Hello ${req.body.user_name}! Your random name is: *${randomAdj} ${randomNoun}*`
 		res.type('application/json').json(slackResponse).end()
 	})
 
