907dfb1b07688dd369fa9e1f1a50a2969d166f16
Daniel Segovia <daniel@dabici.mx>
2017-07-12 12:31:45 -0500

    set a timeout to prevent excesive ajax calls

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,11 @@
-$('#request-random-button').click(() => { // eslint-disable-line no-undef
-  $.post('/temere', (data) => { // eslint-disable-line no-undef
+/* eslint no-undef: 0 */  // --> OFF
+$('#request-random-button').click(() => {
+  $('#request-random-button').prop('disabled', true);
+  $.post('/temere', (data) => {
     console.log(data.text);
-    $('#random-name-field').val(data.text); // eslint-disable-line no-undef
+    $('#random-name-field').val(data.text);
   });
+  setTimeout(() => {
+    $('#request-random-button').prop('disabled', false);
+  }, 1000);
 });
