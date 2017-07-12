9427a5b48f8116ca0594e77c4ba12d02ba05dcc3
Daniel Segovia <daniel@dabici.mx>
2017-07-12 12:12:29 -0500

    eslint

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
-$('#request-random-button').click(() => {
-  $.post('/temere', (data) => {
+$('#request-random-button').click(() => { // eslint-disable-line no-undef
+  $.post('/temere', (data) => { // eslint-disable-line no-undef
     console.log(data.text);
-    $('#random-name-field').val(data.text);
+    $('#random-name-field').val(data.text); // eslint-disable-line no-undef
   });
 });
