bfd119e9fcbaead01ca37a677f0070fcd6a8623e
Daniel Segovia <daniel@dabici.mx>
2017-07-14 18:44:02 -0500

    created with default date

--- app/models/tokens.js
+++ app/models/tokens.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const Token = new Schema({
+  created: {type: Date, default: new Date()},
   access_token: String,
   scope: String,
   user_id: String,
